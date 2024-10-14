ca3af8e06eabddf9e8592341ec415dff9a877f66
Červenec Adam <L20220034@SSPTAJI.CZ>
2024-10-14 12:55:21 +0200

    Adafa

--- logic.js
+++ logic.js
@@ -1,21 +1,55 @@
-function calculateOhmsLaw() {
-    let U = parseFloat(document.getElementById('voltage').value);
-    let I = parseFloat(document.getElementById('current').value);
-    let R = parseFloat(document.getElementById('resistance').value);
+function vypocetohmovazakona() {
+
+    let U = parseFloat(document.getElementById('napeti').value);
+
+    let I = parseFloat(document.getElementById('proud').value);
+
+    let R = parseFloat(document.getElementById('odpor').value);
+
     let result = "";
 
+    var string = "Chyba"
+
+if(R != 0)
+{
+
     if (!isNaN(U) && !isNaN(I)) {
+
         R = U / I;
+
         result = "Odpor (R) = " + R.toFixed(2) + " Ω";
+
+        document.getElementById('odpor').value = R.toFixed(2);
+
     } else if (!isNaN(U) && !isNaN(R)) {
+
         I = U / R;
+
         result = "Proud (I) = " + I.toFixed(2) + " A";
+
+        document.getElementById('proud').value = I.toFixed(2);
+
     } else if (!isNaN(I) && !isNaN(R)) {
+
         U = I * R;
+
         result = "Napětí (U) = " + U.toFixed(2) + " V";
+
+        document.getElementById('napeti').value = U.toFixed(2);
+
     } else {
+
         result = "Prosím zadejte dvě hodnoty pro výpočet.";
+
     }
 
+
     document.getElementById('result').innerHTML = result;
+}
+else
+{
+    chyba = "Nelze delit nulou."
+    
+}
+
 }
